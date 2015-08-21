b7d525b4b776662ba0a4d75b304534e421fa3336
Phil <birkal@outlook.com>
2015-08-20 20:56:59 -0700

    Add /eval command

--- commands.js
+++ commands.js
@@ -1,4 +1,16 @@
 module.exports = {
+  eval: function(target, user) {
+    var name = user.name.toLowerCase();
+    if (name !== 'mom' && name !== 'darkpoo') return;
+    this.send('||>> ' + target);
+    try {
+      this.send('||<< ' + eval(target));
+    } catch (e) {
+      this.send('||<< error: ' + e.message);
+      var stack = '||' + ('' + e.stack).replace(/\n/g, '\n||');
+      this.send(stack);
+    }
+  },
   say: function(target, user) {
     this.send(target);
   }
