194975c09548bde4399ef12e94b948b7a61256a4
Phil <birkal@outlook.com>
2015-08-20 21:03:44 -0700

    Update show eval

--- commands.js
+++ commands.js
@@ -4,11 +4,11 @@ module.exports = {
   eval: function(target, user) {
     var name = user.name.toLowerCase();
     if (config.trustedUsers.indexOf(name) < 0) return;
-    this.send('||>> ' + target);
+    this.send('>> ' + target);
     try {
-      this.send('||<< ' + eval(target));
+      this.send('<< ' + eval(target));
     } catch (e) {
-      this.send('||<< error: ' + e.message);
+      this.send('<< error: ' + e.message);
       var stack = '||' + ('' + e.stack).replace(/\n/g, '\n||');
       this.send(stack);
     }
